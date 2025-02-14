37756b208ee96e60cbf5542723facf263f5ba726
Ramy Fakhoury <Ramy.fakhoury@vastdata.com>
2025-02-14 21:05:49 +0200

    hammerspoon cleans.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,6 +4,8 @@ local log = hs.logger.new('init.lua', 'debug')
 -- hs.hotkey.bind({'ctrl'}, '`', nil, function()
 --   hs.reload()
 -- end)
+
+
 hs.hotkey.bind({ "cmd", "ctrl" }, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -54,15 +56,6 @@ hs.hotkey.bind({ "cmd", "ctrl" }, "J", function()
   win:setFrame(f)
 end)
 
--- hs.hotkey.bind({"cmd", "ctrl"}, "return", function()
---   local win = hs.window.focusedWindow()
---   local screen = win:screen()
---   local max = screen:frame()
---
---   win:setFrame(max)
--- end)
---
-local doublePress = false
 
 -- Function to center window and make it 1/4 of the screen size
 local function centerQuarterScreen()
@@ -81,6 +74,7 @@ local function centerQuarterScreen()
 end
 
 -- Hotkey for cmd + alt + return (enter)
+local doublePress = false
 hs.hotkey.bind({"cmd", "ctrl"}, "return", function()
   if doublePress then
     centerQuarterScreen()  -- Perform the action on second press
@@ -97,41 +91,6 @@ hs.hotkey.bind({"cmd", "ctrl"}, "return", function()
     end)
   end
 end)
-
-keyUpDown = function(modifiers, key)
-  -- Un-comment & reload config to log each keystroke that we're triggering
-  -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
-
-  hs.eventtap.keyStroke(modifiers, key, 0)
-end
-
--- Subscribe to the necessary events on the given window filter such that the
--- given hotkey is enabled for windows that match the window filter and disabled
--- for windows that don't match the window filter.
---
--- windowFilter - An hs.window.filter object describing the windows for which
---                the hotkey should be enabled.
--- hotkey       - The hs.hotkey object to enable/disable.
---
--- Returns nothing.
-enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
-  windowFilter:subscribe(hs.window.filter.windowFocused, function()
-    hotkey:enable()
-  end)
-
-  windowFilter:subscribe(hs.window.filter.windowUnfocused, function()
-    hotkey:disable()
-  end)
-end
-
--- require('keyboard.control-escape')
--- require('keyboard.delete-words')
--- require('keyboard.hyper')
--- require('keyboard.markdown')
--- require('keyboard.panes')
-require('windows')
-
-hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()
 
 -- move application to next screen
 function moveToNextScreen()
